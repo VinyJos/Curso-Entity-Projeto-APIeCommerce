0acb00ef190b0aaa472f1fdc2dc5d427ae53dfd2
Vinicius Jose <viniciuswk@gmail.com>
2023-01-06 22:57:07 -0300

    Conexão com o banco, sem distinção de ambientes de execução

---
+++ eCommerce.API/Database/eCommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        #region Conexão com o banco, sem distinção de ambientes de execução
+        // Conexão com o banco, sem distinção de ambientes de execução
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //qual banco de dados vai ser utilizado
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True");
+
+        }
+        #endregion
+
+        //Conexão com o banco, usando ambientes de desenvolvimento
+
+    }
+}
